6c2f7eebd3d85c7c6da47dbfd8fc78d9d425a762
dietschin <34035069+schynriu@users.noreply.github.com>
2019-11-26 09:54:21 +0100

    Test_Vecteur_DV_26_11_2019

---
+++ Vecteur/Vecteur/Vecteur.cpp
@@ -0,0 +1,58 @@
+
+#include <iostream>
+
+int main()
+{
+	/*Question a
+		//declaration classe vecteur
+
+		class vecteur {
+		int nX, nY, nZ;
+		public:
+			vecteur();
+			vecteur(int, int, int);
+	};
+
+	vecteur::vecteur() {
+		nX = 0; nY = 0; nZ = 0;
+	}
+
+	vecteur::vecteur(int c1, int c2, int c3)
+	{
+		nX = c1; nY = c2, nZ = c3
+	}*/
+
+
+	/* Question B
+
+//Constructeur
+class vecteur
+{
+	int nX, nY, nZ;
+public:
+	vecteur(int = 0, int = 0, int = 0); // constructeur
+
+};
+
+vecteur::vecteur(int c1, int c2, int c3) {
+	nX = c1, nY = c2, nZ = c3;
+	}
+	*/
+
+
+
+
+    /* Question C
+
+	//Constructeur
+		class vecteur
+		{
+			int nX, nY, nZ;
+		public:
+			vecteur(int c1= 0, int c2= 0, int c3= 0); // constructeur
+			nX = c1, nY = c2, nZ = c3;
+	};
+	*/
+}
+
+			
